e14e14801599a0b2b4cbabf3500dc9947a6d1a47
hiladatala <55530946+hiladatala@users.noreply.github.com>
2025-02-14 15:25:50 +0200

    Update train.py

--- train.py
+++ train.py
@@ -195,33 +195,27 @@ class LungSegmentationDataset(Dataset):
         mask = nib.load(self.mask_paths[idx]).get_fdata()  # Load mask as numpy array
         mask = zoom(mask,(512/mask.shape[0], 512/mask.shape[1],115/mask.shape[2]))
         
-        '''
         # Normalize image to zero mean and unit variance
         image = (image - np.mean(image)) / np.std(image)
 
         # Ensure the mask is binary (either 0 or 1)
         mask = np.where(mask > 0.1, 1, 0).astype(np.float32)
-        '''
-
+        
         num_slices = image.shape[2]  # Assuming that slices are along the 3rd dimension
         
         # Lists to hold image and mask slices
-        image_slices = []
-        mask_slices = []
-
+        image_slices = np.empty((num_slices, image.shape[0], image.shape[1]), dtype=np.float32)
+        mask_slices = np.empty((num_slices, mask.shape[0], mask.shape[1]), dtype=np.float32)
+        
         for slice_idx in range(num_slices):
-            image_slice = image[:, :, slice_idx]
-            mask_slice = mask[:, :, slice_idx]
+            image_slice[slice_idx] = image[:, :, slice_idx]
+            mask_slice[slice_idx] = mask[:, :, slice_idx]
 
-            image_slices.append(image_slice)
-            mask_slices.append(mask_slice)
-
-            image_slice = (image_slice - np.mean(image_slice)) / np.std(image_slice)
-            mask_slice = np.where(mask_slice > 0.1, 1, 0).astype(np.float32)
+            #image_slices.append(image_slice)
+            #mask_slices.append(mask_slice)
 
         image_slices = torch.tensor(image_slices, dtype=torch.float32).unsqueeze(1)  # Shape: [115, 1, H, W]
         mask_slices = torch.tensor(mask_slices, dtype=torch.float32).unsqueeze(1)  # Shape: [115, 1, H, W]
-        
         
         '''
         num_batches = int(num_slices//self.batch_size)

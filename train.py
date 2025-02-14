eec3e02f79859e9971aee75704e2cc32ee4d51e7
hiladatala <55530946+hiladatala@users.noreply.github.com>
2025-02-14 02:21:34 +0200

    Update train.py

--- train.py
+++ train.py
@@ -225,7 +225,7 @@ class LungSegmentationDataset(Dataset):
         image_slices = torch.stack(image_slices)
         mask_slices = torch.stack(mask_slices)
 
-        num_batches = int(num_slices//batch_size)
+        num_batches = int(num_slices//self.batch_size)
         image_batches = []
         mask_batches = []
 

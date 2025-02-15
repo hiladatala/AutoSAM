60c68e6a60919e9f202b4f2bd586c83c8296a017
hiladatala <55530946+hiladatala@users.noreply.github.com>
2025-02-15 19:18:54 +0200

    Update train.py

--- train.py
+++ train.py
@@ -206,14 +206,19 @@ class LungSegmentationDataset(Dataset):
         # Lists to hold image and mask slices
         image_slices = np.empty((num_slices, image.shape[0], image.shape[1]), dtype=np.float32)
         mask_slices = np.empty((num_slices, mask.shape[0], mask.shape[1]), dtype=np.float32)
-        
+
+        '''
         for slice_idx in range(num_slices):
             image_slices[slice_idx] = image[:, :, slice_idx]
             mask_slices[slice_idx] = mask[:, :, slice_idx]
 
             #image_slices.append(image_slice)
             #mask_slices.append(mask_slice)
+        '''
+        image_slices = image[:, :, 89:92]
+        mask_slices = mask[:, :, 89:92]
 
+        
         image_slices = torch.tensor(image_slices, dtype=torch.float32) # Shape: [115, 1, H, W]
         mask_slices = torch.tensor(mask_slices, dtype=torch.float32)  # Shape: [115, 1, H, W]
         
